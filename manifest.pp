bf116fa6fc06d1a62cacf925afe0e14c821d6b98
desog <201503692@ingenieria.usac.edu.gt>
2020-12-14 17:39:30 -0600

    Cambios en Manifest.pp

--- manifest.pp
+++ manifest.pp
@@ -7,5 +7,5 @@ include 'docker'
   docker::run{'pruebas':
      image      => 'desog1/practicasa',
      ports      => ['3000'],
-     exponse    => ['3000:8081'],
+     expose     => ['3000:8081'],
   }
